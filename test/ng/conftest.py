bdc40503c81c43bc61063cb09e7863f2cae41594
Frantisek Hrbata <frantisek.hrbata@espressif.com>
2023-12-21 16:51:42 +0100

    ci(ng): add test_parser test for link map file testing
    This simple test just tries to run esp-idf-size on map files
    found in the testing repo under test_parser directory. The purpose
    is to test that the parser can handle all kinds of map files correctly.
    For more info please see
    
    https://github.com/espressif/esp-idf-size-test/tree/master/test_parser
    
    Signed-off-by: Frantisek Hrbata <frantisek.hrbata@espressif.com>

--- test/ng/conftest.py
+++ test/ng/conftest.py
@@ -1,8 +1,29 @@
-from pytest import Parser
+from pathlib import Path
+from subprocess import run
+from tempfile import TemporaryDirectory
+
+import pytest
 
 
-def pytest_addoption(parser: Parser) -> None:
+def pytest_addoption(parser: pytest.Parser) -> None:
     parser.addoption('--url',
                      default='https://github.com/espressif/esp-idf-size-test.git',
                      metavar='URL',
                      help='git URL from which the build artifacts should be fetched')
+
+
+@pytest.fixture
+def artifacts(pytestconfig: pytest.Config, ctx: dict={'tmpdir': None}) -> Path:
+    # Download artifacts for testing into temporary directory and return
+    # the directory path.
+    if ctx['tmpdir']:
+        return Path(ctx['tmpdir'].name)
+
+    tmp_dir = TemporaryDirectory()
+    tmp_dir_path = Path(tmp_dir.name)
+    ctx['tmpdir'] = tmp_dir
+
+    url = pytestconfig.getoption('url')
+    run(['git', 'clone', '--quiet', '--depth', '1', '--single-branch', url, tmp_dir_path], check=True)
+
+    return tmp_dir_path
---
+++ test/ng/test_parser.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+from pathlib import Path
+from subprocess import run
+
+
+def test_parser(artifacts: Path) -> None:
+    # Test that each link map file stored in artifacts repo
+    # under the test_parser directory can be properly parsed.
+    for entry in (artifacts / 'test_parser').glob('*.map'):
+        run([sys.executable, '-m', 'esp_idf_size', '--ng', entry], check=True)
